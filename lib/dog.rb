9a3d2b39ccd0ff9dc5be100bf40726ef0e91784b
Sean Tarzy <seantarzy@gmail.com>
2019-11-19 16:52:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Dog 
+  def initialize(dog_name, dog_breed = "Mutt")
+  @name = dog_name
+  @breed = dog_breed
+end 
+
+def name=(dog_name)
+  @name = dog_name 
+end 
+
+def name 
+  @name 
+end 
+
+def breed= (dog_breed)
+  @breed = dog_breed
+end 
+
+def breed 
+  @breed
+end 
+end 
+
+spot = Dog.new("spot")
